abfe6f9b7d1a4e3fbc3760dff68e5504e7f4b68b
TwistedKnee <daviddrost09@gmail.com>
2022-10-16 16:04:01 -0700

    Initial creation
    Raditz reflection loader created pulling in a HelloWorld Assembly to execute

---
+++ CSharp Loader/Raditz/Raditz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Raditz
+{
+    class Program
+    {
+        static void Reflect(string FilePath)
+        {
+            Assembly dotNetProgram = Assembly.LoadFile(FilePath);
+            Object[] parameters = new String[] { null };
+            dotNetProgram.EntryPoint.Invoke(null, parameters);
+        }
+        static void Main(string[] args)
+        {
+            Reflect(@"C:\Users\User\Documents\GitHub\LearnCSharp\CSharp Loader\HelloFromReflectionWorld\HelloFromReflectionWorld\bin\Release\HelloFromReflectionWorld.exe");
+        }
+    }
+}
